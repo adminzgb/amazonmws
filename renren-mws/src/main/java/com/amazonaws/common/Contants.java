8c4797f5d0e2bdfa9ba2f33a21ba100539512f03
adminzgb <821105910@qq.com>
2017-05-08 08:10:24 +0800

    新增amazon模块

---
+++ renren-mws/src/main/java/com/amazonaws/common/Contants.java
@@ -0,0 +1,14 @@
+package com.amazonaws.common;
+
+public class Contants {
+
+	public static final String ACCESS_KEY_ID = "AKIAJDQFOB6CWVKDKYXQ";
+
+	public static final String SELLER_ID = "A1WUZSL0EJXK6U";
+	
+	public static final String SECRET_ACCESS_KEY = "+ZVwH6QnatKrmUbcFmULjQAIgGg1IzIK1BOWDcJU";
+	
+	public static final String APP_NAME = "QBMWS";
+	
+	public static final String APP_VERSION = "0.0.1";
+}
